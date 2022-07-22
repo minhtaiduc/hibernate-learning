d48facfbc2a0294e970b99282c3360f702e64d39
mtd <minhtaiduc@gmail.com>
2022-07-22 09:33:57 +0700

    Commit after reinstalled OS

---
+++ src/main/sql/initial.sql
@@ -0,0 +1,11 @@
+create database hibernate_learning;
+
+use hibernate_learning;
+
+create table EMPLOYEE (
+                          id INT NOT NULL auto_increment,
+                          first_name VARCHAR(20) default NULL,
+                          last_name VARCHAR(20) default NULL,
+                          salary INT default NULL,
+                          PRIMARY KEY (id)
+)
